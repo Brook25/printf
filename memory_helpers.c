7580b2d74dd88e6e89edd01ef91eb7da87529ade
biruk-tadesse <biruk.tadesse.tefera@gmail.com>
2022-05-17 14:36:17 +0000

    editing files and tidy up

--- memory_helpers.c
+++ memory_helpers.c
@@ -37,8 +37,8 @@ unsigned int _memcpy(buffer_t *output, const char *src, unsigned int n)
 }
 
 /**
- * free_buffer - Frees a buffer_t struct.
- * @output: The buffer_t struct to be freed.
+ * free_buffer - Frees a buffer_t struct
+ * @output: The buffer_t struct to be freed
  */
 void free_buffer(buffer_t *output)
 {
